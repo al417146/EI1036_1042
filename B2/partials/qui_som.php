d2725b77fb2a5a11b3618793b2e2d8b2035691df
PauE <al417146@uji.es>
2025-09-25 18:19:52 +0200

    Fin clase practica 2

---
+++ B2/partials/qui_som.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Qui Som</title>
+
+</head>
+
+<body>
+    <h1>a = cos(x) + isin(x) es compacto</h1>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194347.47968121845!2d-3.8446863160840934!3d40.438098122153434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2sMadrid!5e0!3m2!1ses!2ses!4v1758815305480!5m2!1ses!2ses" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></body>
+
+
+
+</body>    
+
+</html>
